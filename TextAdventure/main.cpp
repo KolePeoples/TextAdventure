5ff31d274ae4d27a630057f391a38767c4380902
kolepeoples <kolepeoples@gmail.com>
2024-01-23 15:42:05 -0600

    Initial Commit

---
+++ TextAdventure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TextAdventure
+//
+//  Created by Kole Peoples on 1/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
