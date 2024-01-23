1987cbbef5d0f63f87e178033e9057663e4f3cad
kolepeoples <kolepeoples@gmail.com>
2024-01-23 15:45:03 -0600

    Initial Commit

--- TextAdventure/main.cpp
+++ TextAdventure/main.cpp
@@ -6,9 +6,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    int location = 0;
+    bool  gameRunning = true;
+    
+    while(gameRunning) {
+        
+    }
 }
+
